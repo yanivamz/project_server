2dd3116dc71dae490fa6bd7feaf6035c3914c276
Yaniv Amzaleg <yanivamz@gmail.com>
2023-08-03 19:59:37 +0300

    add mongodb conect

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const path = require('path');
 const cors = require('cors')
 const app = express();
 const port = 5000;
+const mongoose = require('mongoose');
+const Singer = require('./Singer')
+
+
+const url = 'mongodb+srv://yanivamz:macabi11@cluster0.aidqfhh.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(url);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
@@ -16,3 +22,6 @@ app.get('/test', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const singers = new Singer ({ name: 'david', age: 25});
+singers.save().then(() => console.log ('singer added..'));
